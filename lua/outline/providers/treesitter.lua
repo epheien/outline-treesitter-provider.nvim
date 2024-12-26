2a1bd5ea82b1fa73e3923b6a4bcc799c70994932
eph <eph@MacBook-Pro.local>
2024-12-26 20:48:44 +0800

    remove man support

--- lua/outline/providers/treesitter.lua
+++ lua/outline/providers/treesitter.lua
@@ -6,18 +6,18 @@ local M = {
 
 local ft_to_parser_name = {
   help = "vimdoc",
-  man = 'man', -- hack
+  --man = 'man', -- hack
 }
 
 ---@return boolean, table?
 function M.supports_buffer(bufnr)
-  bufnr = bufnr ~= 0 and bufnr or vim.api.nvim_get_current_buf()
+  --bufnr = bufnr ~= 0 and bufnr or vim.api.nvim_get_current_buf()
   local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
   local parser_name = ft_to_parser_name[ft]
   if parser_name == nil then return false end
-  if ft == 'man' then
-    return true, { ft = ft, buf = bufnr, lang = parser_name }
-  end
+  --if ft == 'man' then
+  --  return true, { ft = ft, buf = bufnr, lang = parser_name }
+  --end
   local ok, parser = pcall(vim.treesitter.get_parser, bufnr, parser_name)
   if not ok then return false end
   return true, { parser = parser, ft = ft, buf = bufnr, lang = parser_name }
